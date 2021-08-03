628a1691845e9036ec431cb7435b7ee340e81f0d
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-08-03 17:14:00 +0530

    Refactor: made several changes
    App is ready now!

---
+++ index.js
@@ -0,0 +1,64 @@
+const inputs= document.querySelectorAll(".input");
+const form = document.forms[0];
+const outputDiv= document.querySelector(".output");
+const motion= document.querySelector("#motion");
+
+form.addEventListener("submit", checkcalcHandler);
+
+
+
+function checkcalcHandler(e){
+    e.preventDefault();
+
+     var cp= inputs[0].value;
+     var quantity= inputs[1].value;
+     var sell= inputs[2].value;
+     if( !isNaN(cp) && !isNaN(quantity) && !isNaN(sell)){
+      cp= Number(cp);
+      quantity= Number(quantity);
+      sell= Number(sell);
+
+      if( cp>0 && quantity>0 && sell>0){
+          if(cp>sell){
+              var loss= ((cp-sell)*quantity).toFixed(2);
+              var losspercent= (((cp-sell)*100)/cp).toFixed(2);
+              outputDiv.innerHTML= `<div> You lost ${losspercent}%. Your total loss is ₹${loss}. </div>`;
+              motion.innerHTML = "<img src='images/poverty.jpg' width='350px' height='300px' />"
+    
+            
+
+
+              
+          }
+      
+      else{
+        var profit = ((sell-cp)*quantity).toFixed(2)
+        var profitPercent=(((sell-cp)*100)/sell).toFixed(2) ;
+        outputDiv.innerHTML=  `<div> You gained ${profitPercent}%. Your total profit is ₹${profit}</div>`;
+        motion.innerHTML = "<img src='images/rich.jpg' width='400px' height='300px' />"
+       
+        
+      
+       
+    }}
+ else {
+        output.innerHTML= "Please Enter values greater than 0 that is positive value";
+    }
+
+     
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
